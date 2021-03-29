ba2bb549cc4cb57eb554a1f7bb62dbe9007d2a92
shpeeck <jenya_vas@mail.ru>
2021-03-29 17:47:45 +0300

    add position.pdf and modal

--- js/main.js
+++ js/main.js
@@ -1,26 +1,55 @@
 
+try {
+    const swiper = new Swiper('.swiper-container', {
+        // Optional parameters
+        loop: true,
+        speed: 300,
+        autoplay: {
+            delay: 5000,
+        },
+      
+        // If we need pagination
+        pagination: {
+          el: '.swiper-pagination',
+        },
+      
+        // Navigation arrows
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },
+      
+        // And if we need scrollbar
+        scrollbar: {
+          el: '.swiper-scrollbar',
+        },
+      });
+} catch (error) {
+    
+}
 
-const swiper = new Swiper('.swiper-container', {
-    // Optional parameters
-    loop: true,
-    speed: 300,
-    autoplay: {
-        delay: 5000,
-    },
-  
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-    },
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  
-    // And if we need scrollbar
-    scrollbar: {
-      el: '.swiper-scrollbar',
-    },
-  });
+
+
+
+
+  // Открытие модалки формы 
+  try {
+    let mod = document.querySelector(".position");
+    let op = document.querySelector(".add");
+    let close = document.querySelector(".position__close");
+    // console.log(op);
+    // console.log(mod);
+
+    // op.forEach(function (item, index) {
+    op.addEventListener("click", function () {
+        mod.classList.add("active");
+        // console.log("нажата")
+    });
+
+    close.addEventListener("click", function () {
+        mod.classList.remove("active");
+    });
+} catch (err) {
+    // console.log(err);
+
+};
