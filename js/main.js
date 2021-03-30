f37da829588ca7517912702fbc3b261ac3457a15
shpeeck <jenya_vas@mail.ru>
2021-03-30 22:22:47 +0300

    add sliders

--- js/main.js
+++ js/main.js
@@ -1,33 +1,274 @@
 
+// swiper sertificats
 try {
-    const swiper = new Swiper('.swiper-container', {
+    const swiper = new Swiper('.sert', {
         // Optional parameters
         loop: true,
         speed: 300,
         autoplay: {
             delay: 5000,
         },
-      
+
         // If we need pagination
         pagination: {
-          el: '.swiper-pagination',
+            el: '.swiper-pagination',
         },
-      
+
         // Navigation arrows
         navigation: {
-          nextEl: '.swiper-button-next',
-          prevEl: '.swiper-button-prev',
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
         },
-      
+
         // And if we need scrollbar
         scrollbar: {
-          el: '.swiper-scrollbar',
+            el: '.swiper-scrollbar',
         },
-      });
+    });
 } catch (error) {
     
 }
 
+// swiper slider
+try {
+    const swiper = new Swiper('.slider', {
+        // Optional parameters
+        loop: true,
+        speed: 300,
+        autoplay: {
+            delay: 5000,
+        },
+
+        // If we need pagination
+        pagination: {
+            el: '.swiper-pagination',
+        },
+
+        // Navigation arrows
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+
+        // And if we need scrollbar
+        scrollbar: {
+            el: '.swiper-scrollbar',
+        },
+    });
+} catch (error) {
+    
+}
+
+// swiper slider 0
+try {
+    const swiper = new Swiper('.slider0', {
+        // Optional parameters
+        loop: true,
+        speed: 300,
+        autoplay: {
+            delay: 5000,
+        },
+
+        // If we need pagination
+        pagination: {
+            el: '.swiper-pagination',
+        },
+
+        // Navigation arrows
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+
+        // And if we need scrollbar
+        scrollbar: {
+            el: '.swiper-scrollbar',
+        },
+    });
+} catch (error) {
+    
+}
+
+// swiper slider-family
+try {
+    const swiper = new Swiper('.slider-family', {
+        // Optional parameters
+        loop: true,
+        speed: 300,
+        autoplay: {
+            delay: 5000,
+        },
+
+        // If we need pagination
+        pagination: {
+            el: '.swiper-pagination',
+        },
+
+        // Navigation arrows
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+
+        // And if we need scrollbar
+        scrollbar: {
+            el: '.swiper-scrollbar',
+        },
+    });
+} catch (error) {
+    
+}
+
+// swiper 1
+try {
+    const swiper = new Swiper('.slider1', {
+        // Optional parameters
+        loop: true,
+        speed: 300,
+        autoplay: {
+            delay: 5000,
+        },
+
+        // If we need pagination
+        pagination: {
+            el: '.swiper-pagination',
+        },
+
+        // Navigation arrows
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+
+        // And if we need scrollbar
+        scrollbar: {
+            el: '.swiper-scrollbar',
+        },
+    });
+} catch (error) {
+    
+}
+
+// swiper 2
+try {
+    const swiper = new Swiper('.slider2', {
+        // Optional parameters
+        loop: true,
+        speed: 300,
+        autoplay: {
+            delay: 5000,
+        },
+
+        // If we need pagination
+        pagination: {
+            el: '.swiper-pagination',
+        },
+
+        // Navigation arrows
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+
+        // And if we need scrollbar
+        scrollbar: {
+            el: '.swiper-scrollbar',
+        },
+    });
+} catch (error) {
+    
+}
+
+// swiper 3
+try {
+    const swiper = new Swiper('.slider3', {
+        // Optional parameters
+        loop: true,
+        speed: 300,
+        autoplay: {
+            delay: 5000,
+        },
+
+        // If we need pagination
+        pagination: {
+            el: '.swiper-pagination',
+        },
+
+        // Navigation arrows
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+
+        // And if we need scrollbar
+        scrollbar: {
+            el: '.swiper-scrollbar',
+        },
+    });
+} catch (error) {
+    
+}
+
+// swiper 4
+try {
+    const swiper = new Swiper('.slider4', {
+        // Optional parameters
+        loop: true,
+        speed: 300,
+        autoplay: {
+            delay: 5000,
+        },
+
+        // If we need pagination
+        pagination: {
+            el: '.swiper-pagination',
+        },
+
+        // Navigation arrows
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+
+        // And if we need scrollbar
+        scrollbar: {
+            el: '.swiper-scrollbar',
+        },
+    });
+} catch (error) {
+    
+}
+
+// swiper 5
+try {
+    const swiper = new Swiper('.slider5', {
+        // Optional parameters
+        //width: 1200,
+        loop: true,
+        speed: 300,
+        autoplay: {
+            delay: 5000,
+        },
+
+        // If we need pagination
+        pagination: {
+            el: '.swiper-pagination',
+        },
+
+        // Navigation arrows
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+
+        // And if we need scrollbar
+        scrollbar: {
+            el: '.swiper-scrollbar',
+        },
+    });
+} catch (error) {
+    
+}
 
 
 
